f6b059d07bb22599e8460859df9dd8a353c1f571
Islomkhodja <islomkhodjadev>
2024-12-24 07:51:56 +0500

    corrected urls.py added / to order url

--- erp/main/urls.py
+++ erp/main/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('order/<str:language>', views.order_view, name='order_form'), 
+    path('order/<str:language>/', views.order_view, name='order_form'), 
     path("get_plintus_component/<int:plintus_code>/", views.get_components_by_plintus_code, name='getplintus_component'),
     
 ]
