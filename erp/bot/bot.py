10435faee95300be6ffb250472ca1111bba67e46
Islomkhodja <islomkhodjadev>
2025-01-06 19:43:51 +0500

    1

--- erp/bot/bot.py
+++ erp/bot/bot.py
@@ -50,6 +50,7 @@ import markup
 # Handle start command
 @dp.message(or_f(CommandStart(), Command("login")))
 async def command_start(message: Message, state: FSMContext) -> None:
+    await state.clear()
     await models.save_telegram_user_data(message)
     await state.set_state(Login.username)
     await message.answer(text="Iltimos, foydalanuvchi nomini kiriting 🇺🇿\n"
